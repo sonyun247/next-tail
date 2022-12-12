daac5ef4d5ffacb6573aca094428cb675e53aa8e
LEE Seungje <sonyun24@gmail.com>
2022-12-12 16:58:41 +0900

    Design: projects link
    프리뷰 이미지 링크에서 제목 링크로 변경

--- components/card.tsx
+++ components/card.tsx
@@ -13,22 +13,22 @@ interface ICard {
 export default function Card({ data }: ICard) {
   return (
     <article className="p-7 flex flex-col items-center">
-      <header className="pb-2 text-3xl font-semibold dark:text-blue-400 tracking-wider">
-        <h2>{data?.title}</h2>
+      <header className="pb-2 text-3xl font-semibold dark:text-blue-400 hover:dark:text-blue-500 hover:text-blue-400 hover:dark:underline hover:underline tracking-wider">
+        <Link href={data?.url} target="_blank">
+          <h2>{data?.title}</h2>
+        </Link>
       </header>
       <div className="group flex flex-col relative justify-center items-center">
-        <Link href={data?.url} target="_blank">
-          <Image
-            src={data?.image}
-            alt={data?.title}
-            priority
-            width={500}
-            height={500}
-            placeholder="blur"
-            blurDataURL={data?.image}
-            className="rounded-t-3xl rounded-l-3xl group-hover:brightness-50"
-          />
-        </Link>
+        <Image
+          src={data?.image}
+          alt={data?.title}
+          priority
+          width={500}
+          height={500}
+          placeholder="blur"
+          blurDataURL={data?.image}
+          className="rounded-t-3xl rounded-l-3xl group-hover:brightness-50"
+        />
         <div className="group-hover:visible invisible absolute leading-loose font-bold italic text-xl text-white dark:text-white text-center">
           <p>{data?.description}</p>
         </div>
